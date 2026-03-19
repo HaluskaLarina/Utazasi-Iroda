14d17fb0dd61eaa11dc8424088dad16d4f8ce1fd
HaluskaLarina <haluska.larina-2023@keri.mako.hu>
2026-03-19 09:43:54 +0100

    swiper

--- script/script.js
+++ script/script.js
@@ -7,7 +7,6 @@ fetch("header.html")
 })
 .catch(error => console.error("Hiba a menü betöltődésekor!", error));
 
-<<<<<<< HEAD
 
 const elemek = {
     helyMenu: null,
@@ -16,7 +15,6 @@ const elemek = {
     toast: null,
     toastMessage: null
 };
-=======
 //footer betoltese
 fetch("footer.html") 
 .then(response => response.text())
@@ -25,4 +23,38 @@ fetch("footer.html")
     if (el) el.innerHTML = data;
 })
 .catch(error => console.error("Hiba a lábléc betöltődésekor!", error));
->>>>>>> 686ec3d736c9692d996b8b6920ae123b922a1eb3
+
+
+/*
+inspiration
+https://cz.pinterest.com/pin/830703093792161391/
+*/
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5
+  },
+  keyboard: {
+    enabled: true
+  },
+  mousewheel: {
+    thresholdDelta: 70
+  },
+  spaceBetween: 30,
+  loop: false,
+  breakpoints: {
+    640: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    }
+  }
+});
+
+swiper.slideTo(1, false, false);
