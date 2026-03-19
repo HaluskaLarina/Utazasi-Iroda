686ec3d736c9692d996b8b6920ae123b922a1eb3
Milanx2008 <bombolya.milan-2023@keri.mako.hu>
2026-03-19 09:13:01 +0100

    2026.03.19 9:12

--- script/script.js
+++ script/script.js
@@ -1,11 +1,17 @@
-let helyek = [];
-let szurtHelyek = [];
+//header betoltese
+fetch("header.html")
+.then(response => response.text())
+.then(data => {
+    let he = document.getElementById("js-navbar");
+    if (he) he.innerHTML = data;
+})
+.catch(error => console.error("Hiba a menü betöltődésekor!", error));
 
-
-const elemek = {
-    helyMenu: null,
-    helyekSzama: null,
-    osszesHely: null,
-    toast: null,
-    toastMessage: null
-};
+//footer betoltese
+fetch("footer.html") 
+.then(response => response.text())
+.then(data => {
+    let el = document.getElementById("js-footer");
+    if (el) el.innerHTML = data;
+})
+.catch(error => console.error("Hiba a lábléc betöltődésekor!", error));
