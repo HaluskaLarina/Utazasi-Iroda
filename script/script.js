60024ac99a3473d8bf6c4784c17b27793d701155
HaluskaLarina <haluska.larina-2023@keri.mako.hu>
2026-03-20 10:39:51 +0100

    fed

--- script/script.js
+++ script/script.js
@@ -16,14 +16,46 @@ const elemek = {
     toastMessage: null
 };
 //footer betoltese
-fetch("footer.html") 
-.then(response => response.text())
-.then(data => {
-    let el = document.getElementById("js-footer");
-    if (el) el.innerHTML = data;
-})
-.catch(error => console.error("Hiba a lábléc betöltődésekor!", error));
+fetch("footer.html") // betölti az oldalt
+    .then(response => response.text()) // a fetch amikor visszaad egy választ (response), akkor lefut egy függvény, ami a response.text() értéket adja vissza
+    .then(data => {
+        let el = document.getElementById("js-footer").innerHTML = data;
+        if (el) el.innerHTML = data;
+    })
+    .catch(error => console.error("Hiba a lábléc betöltődésekor!", error));
 
+
+    setTimeout(() => {
+  let email2Input = document.getElementById("email2");
+  let gomb2 = document.getElementById("feliratkozas2");
+  let uzenet2 = document.getElementById("uzenet2");
+
+  // Regex az e-mail ellenőrzéshez
+  //let emailMinta = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  let email2Pattern = /^([a-z0-9_.+-])+\@(([a-z0-9-])+\.)+([a-z0-9]{2,4})+$/;
+
+  if (email2Input && gomb2 && uzenet2) {
+    email2Input.addEventListener("input", () => {
+      //if (emailMinta.test(emailInput.value)) {
+      if (email2Pattern.test(email2Input.value)) {
+        gomb2.disabled = false;
+        uzenet2.textContent = "Helyes e-mail formátum ✅";
+        uzenet2.style.color = "lightgreen";
+      } else {
+        gomb2.disabled = true;
+        uzenet2.textContent = "Érvénytelen e-mail cím.❌ Adjon meg egy érvényes e-mail címet";
+        uzenet2.style.color = "darkred";
+      } 
+    });
+
+    gomb2.addEventListener("click", () => {
+      alert("Köszönjük a feliratkozást!");
+      uzenet2.textContent = "";
+      email2Input.value = "";
+      gomb2.disabled = true;
+  });
+}
+}, 100);
 
 
 var swiper = new Swiper(".swiper", {
@@ -55,3 +87,9 @@ var swiper = new Swiper(".swiper", {
 });
 
 swiper.slideTo(1, false, false);
+
+// kapcs
+document.querySelector(".contact-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Üzenet elküldve! (demo)");
+});
